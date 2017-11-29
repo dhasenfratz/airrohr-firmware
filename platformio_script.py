b2d196df12058fa530e0e9d58fca8f6305da9720
dhasenfratz <david.hasenfratz@gmail.com>
2017-11-29 22:23:22 +0100

    Add version number to the build file name

--- platformio_script.py
+++ platformio_script.py
@@ -2,10 +2,11 @@
 Import("env")
 import os
 import shutil
-from base64 import b64decode
+import subprocess
 
 def after_build(source, target, env):
-    shutil.copy(target[0].path, "./builds/latest_"+b64decode(ARGUMENTS.get("LANG"))+".bin")
+    version = subprocess.check_output(["git", "describe"]).strip()
+    shutil.copy(target[0].path, "./builds/airrohr-fw-" + version + ".bin")
 
 
 env.AddPostAction("$BUILD_DIR/firmware.bin", after_build)
